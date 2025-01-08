9fd84b86f13821183615d85c3511bbc613ecd72d
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-07 21:05:23 -0600

    Remove success messages from book creation, update, and deletion responses

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -20,18 +20,18 @@ class BookController extends Controller
     public function store(BookRequest $request)
     {
         Book::create($request->validated());
-        return redirect()->route('books.index')->with('success', 'Libro Creado.');
+        return redirect()->route('books.index');
     }
 
     public function update(BookRequest $request, Book $book)
     {
         $book->update($request->validated());
-        return redirect()->route('books.index')->with('success', 'Libro Actualizado.');
+        return redirect()->route('books.index');
     }
 
     public function destroy(Book $book)
     {
         $book->delete();
-        return redirect()->route('books.index')->with('success', 'Libro Eliminado.');
+        return redirect()->route('books.index');
     }
 }
